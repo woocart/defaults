14fc681926861386c70776903731f55597a14c32
Janez Troha <dz0ny@ubuntu.si>
2019-02-14 11:14:45 +0100

    Update PR Action
    Add branch creation

--- .github/create_pr.py
+++ .github/create_pr.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     if not event.is_release:
         raise Exception("This event is not from release")
 
-    with commit(event, f"update/wd_{event.release.tag_name}", env["PTA_TOKEN"]) as gh:
-        versions = gh.get("niteoweb/woocart-docker-web", "bin/runtime/versions")
+    with commit("niteoweb/woocart-docker-web", f"update/wd_{event.release.tag_name}", env["PTA_TOKEN"]) as gh:
+        versions = gh.get("bin/runtime/versions")
         regex = r"\/v(.+)\/"
         versions.text = re.sub(regex, f"/{event.release.tag_name}/", versions.text)
-        gh.add("niteoweb/woocart-docker-web", versions, f"Update woocart-defaults to {event.release.tag_name}")
+        gh.add(versions, f"Update woocart-defaults to {event.release.tag_name}")
         gh.create_pr(
             f"Update woocart-defaults to {event.release.tag_name}",
             f"This updates woocart-defaults to version {event.release.tag_name}",
