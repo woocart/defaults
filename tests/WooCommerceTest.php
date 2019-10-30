29e7cefef31695d6c68890bab1645f0e3e826fdd
Akshit Sethi <akshitsethi@gmail.com>
2019-10-30 14:48:43 +0530

    Add tests for the woocommerce class

---
+++ tests/WooCommerceTest.php
@@ -0,0 +1,64 @@
+<?php
+
+use Niteo\WooCart\Defaults\WooCommerce;
+use PHPUnit\Framework\TestCase;
+
+class WooCommerceTest extends TestCase {
+
+
+	function setUp() {
+		\WP_Mock::setUp();
+	}
+
+	function tearDown() {
+		$this->addToAssertionCount(
+			\Mockery::getContainer()->mockery_getExpectationCount()
+		);
+		\WP_Mock::tearDown();
+		\Mockery::close();
+	}
+
+	/**
+	 * @covers \Niteo\WooCart\Defaults\WooCommerce::__construct
+	 */
+	public function testConstructor() {
+		$woocommerce = new WooCommerce();
+		\WP_Mock::expectFilterAdded( 'woocommerce_general_settings', [ $woocommerce, 'general_settings' ] );
+
+		$woocommerce->__construct();
+		\WP_Mock::assertHooksAdded();
+	}
+
+	/**
+	 * @covers \Niteo\WooCart\Defaults\WooCommerce::__construct
+	 * @covers \Niteo\WooCart\Defaults\WooCommerce::general_settings
+	 */
+	public function testGeneralSettings() {
+		$woocommerce = new WooCommerce();
+
+		$this->assertEquals(
+			$woocommerce->general_settings(
+				[
+					[
+						'id'   => 'general_options',
+						'type' => 'sectionend',
+					],
+				]
+			),
+			[
+				[
+					'name'     => 'Business Name',
+					'desc_tip' => 'Name of the business used for operating the store.',
+					'id'       => 'woocommerce_company_name',
+					'type'     => 'text',
+					'default'  => '',
+				],
+				[
+					'id'   => 'general_options',
+					'type' => 'sectionend',
+				],
+			]
+		);
+	}
+
+}
