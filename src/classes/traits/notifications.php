e1fac1e88534523768ab539f134d2ce4a1a87f69
Akshit Sethi <akshitsethi@gmail.com>
2020-06-08 23:05:23 +0530

    Update notification box design & array_match logic

--- src/classes/traits/notifications.php
+++ src/classes/traits/notifications.php
@@ -22,36 +22,42 @@ namespace Niteo\WooCart\Defaults\Extend {
 				$this->notification['term'] = strip_tags( $args->search );
 
 				// Check for keywords
-				$filter = array_filter(
-					array(
+				$keywords = array(
+					'backup'      => array(
 						'backup',
 						'backwpup',
 						'duplicate',
+					),
+					'security'    => array(
 						'restore',
 						'security',
 						'wordfence',
 						'firewall',
+					),
+					'performance' => array(
 						'cache',
 						'smush',
 						'optimize',
 						'minify',
 						'compress',
 					),
-					array( $this, 'array_match' )
 				);
+
+				// Match for keywords
+				$this->array_match( $keywords );
 
 				if ( $this->notification['matches'] ) {
 					// Add the right notification message
 					if ( 'backup' === $this->notification['matches'] ) {
-						$this->notification['message'] = esc_html__( 'Warning! You are searching for backup plugins. WooCart strongly advises against installing this type of plugins because it may significantly impact staging creation. Use the Backups tab in the WooCart dashboard instead. Contact support for more information.', 'woocart-defaults' );
+						$this->notification['message'] = esc_html__( 'You are searching for backup plugins. WooCart strongly advises against installing this type of plugins because it may significantly impact staging creation. Use the Backups tab in the WooCart dashboard instead. Contact support for more information.', 'woocart-defaults' );
 					}
 
 					if ( 'security' === $this->notification['matches'] ) {
-						$this->notification['message'] = esc_html__( 'Warning! You are searching for security plugins. WooCart strongly advises against installing this type of plugins because it can affect the existing security configuration. Contact support for more information.', 'woocart-defaults' );
+						$this->notification['message'] = esc_html__( 'You are searching for security plugins. WooCart strongly advises against installing this type of plugins because it can affect the existing security configuration. Contact support for more information.', 'woocart-defaults' );
 					}
 
 					if ( 'performance' === $this->notification['matches'] ) {
-							 $this->notification['message'] = esc_html__( 'Warning! You are searching for performance plugins. WooCart strongly advises against installing this type of plugins because it can affect the existing configuration and cause performance issues. Contact support for more information.', 'woocart-defaults' );
+						$this->notification['message'] = esc_html__( 'You are searching for performance plugins. WooCart strongly advises against installing this type of plugins because it can affect the existing configuration and cause performance issues. Contact support for more information.', 'woocart-defaults' );
 					}
 
 					add_action( 'install_plugins_table_header', array( $this, 'add_text' ) );
@@ -65,37 +71,32 @@ namespace Niteo\WooCart\Defaults\Extend {
 		 * @codeCoverageIgnore
 		 */
 		public function add_text() : void {
-			echo '<div class="widefat">';
-			echo '<p style="width:100%;background:rgba(241,130,141,0.70);padding:0.500rem 4px;">';
-			echo $this->notification['message'];
-			echo '</p>';
-			echo '</div>';
+			echo '<div class="notice" style="background: #e3d9f0;
+              border-radius: 8px;
+              border: none;
+              width: 100%;
+              padding: 1em 4px;"><span style="background: orange;
+              border-radius: 5px;
+              color: white;
+              padding: 0.3em;">WARNING</span> <span style="line-height: 22px;
+              font-size: 1.07em;">' . $this->notification['message'] . '</span></div>';
 		}
 
 		/**
 		 * Function to match array of keywords with the search query.
 		 *
-		 * @param string $keyword Keyword to match against
+		 * @param array $keywords Keywords to match against
 		 * @return bool
 		 */
-		private function array_match( string $keyword ) : bool {
-			if ( strpos( $this->notification['term'], $keyword ) !== false ) {
-				// Backup
-				if ( in_array( $keyword, array( 'backup', 'duplicate', 'restore' ) ) ) {
-					$this->notification['matches'] = 'backup';
-				}
+		private function array_match( array $keywords ) : bool {
+			foreach ( $keywords as $key => $value ) {
+				foreach ( $value as $keyword ) {
+					if ( strpos( $this->notification['term'], $keyword ) !== false ) {
+						$this->notification['matches'] = $key;
 
-				// Security
-				if ( in_array( $keyword, array( 'security', 'wordfence', 'firewall' ) ) ) {
-					$this->notification['matches'] = 'security';
+						return true;
+					}
 				}
-
-				// Performance
-				if ( in_array( $keyword, array( 'cache', 'smush', 'optimize', 'minify', 'compress' ) ) ) {
-					$this->notification['matches'] = 'performance';
-				}
-
-				return true;
 			}
 
 			return false;
