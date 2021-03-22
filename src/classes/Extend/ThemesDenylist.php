f2fcde76d814fde8208b0d3dafe460cc7529dac4
Akshit Sethi <akshitsethi@gmail.com>
2021-03-22 19:39:45 +0530

    Fix denylist for themes

--- src/classes/Extend/ThemesDenylist.php
+++ src/classes/Extend/ThemesDenylist.php
@@ -16,6 +16,11 @@ trait ThemesDenylist {
 	public function add_denylist_theme_notice() : void {
 		$current_theme = \wp_get_theme();
 
+		// Check for theme.
+		if ( ! in_array( $current_theme->get( 'Name' ), $this->themes_denylist ) ) {
+			return;
+		}
+
 		$message = sprintf(
 			esc_html__( '%1$s theme has been shown to have poor performance. We recommend switching to a different theme.', 'woocart-defaults' ),
 			"<strong>{$current_theme->get( 'Name' )}</strong>"
